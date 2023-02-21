f414dfd205849c713dccac74557c5e962c92954d
daizdag <115800558+daizdag@users.noreply.github.com>
2023-02-21 20:29:06 +0900

    投稿機能実装

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,6 +1,28 @@
 class CollectionsController < ApplicationController
 
   def index
-    
+    @collections = Collection.all
   end
+
+  def new
+    @collection = Collection.new
+  end
+
+  def create
+    @collection = Collection.new(collection_params)
+    if @collection.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def collection_params
+    params.require(:collection).permit(:title, :point, :content, :category_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 end
---
+++ app/models/collection.rb
@@ -0,0 +1,7 @@
+class Collection < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  belongs_to :user
+  belongs_to :category
+  has_one_attached :image
+end

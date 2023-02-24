eb4c9491c75efb46263d87607244b9038584938c
daizdag <115800558+daizdag@users.noreply.github.com>
2023-02-24 14:37:23 +0900

    作品情報編集機能の実装

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,5 +1,7 @@
 class CollectionsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_collection, only: [:show, :edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @collections = Collection.all
@@ -22,6 +24,17 @@ class CollectionsController < ApplicationController
     @collection = Collection.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    if @collection.update(collection_params)
+      redirect_to collection_path(@collection)
+    else
+      render :edit
+    end
+  end
+
 
 
   private
@@ -30,4 +43,11 @@ class CollectionsController < ApplicationController
     params.require(:collection).permit(:title, :point, :content, :category_id, {images: []}).merge(user_id: current_user.id)
   end
 
+  def set_collection
+    @collection = Collection.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @collection.user
+  end
 end

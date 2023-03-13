e48753714d783479ebe8ac21ac4292ad924430eb
daizdag <115800558+daizdag@users.noreply.github.com>
2023-03-14 00:17:34 +0900

    お気に入りカウント機能

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,10 @@ class Favorite < ApplicationRecord
   belongs_to :user   
   belongs_to :collection   
 
-validates_uniqueness_of :collection_id, scope: :user_id
-
+  validates_uniqueness_of :collection_id, scope: :user_id
+  
+  def self.count_by_collection(collection_id)
+    where(collection_id: collection_id).count
+  end
 
 end
